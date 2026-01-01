268157eca7e4281849da65a35a0bdd74b6646fc6
Deepak Palpandi <deepak.palpandi@dedalus.eu>
2026-01-01 22:19:25 +0530

    Add ControllerExceptionCaptureAttribute for centralized exception handling in controllers

--- Helpers/CommonHelper.cs
+++ Helpers/CommonHelper.cs
@@ -1,3 +1,6 @@
+using System.Security.Cryptography;
+using System.Text;
+
 namespace HeatBeat.Shared.Helpers;
 
 public static class CommonHelper
@@ -11,5 +14,29 @@ public static class CommonHelper
 
         return string.Concat(name.Select((c, i) =>
             i > 0 && char.IsUpper(c) ? "_" + char.ToLowerInvariant(c) : char.ToLowerInvariant(c).ToString()));
+    }
+
+    public static class PasswordHelper
+    {
+        public static (string Hash, string Salt) CreatePassword(string password)
+        {
+            using var hmac = new HMACSHA256();
+            var salt = Convert.ToBase64String(hmac.Key);
+            var hash = Convert.ToBase64String(
+                hmac.ComputeHash(Encoding.UTF8.GetBytes(password))
+            );
+
+            return (hash, salt);
+        }
+
+        public static bool VerifyPassword(string password, string storedHash, string storedSalt)
+        {
+            using var hmac = new HMACSHA256(Convert.FromBase64String(storedSalt));
+            var computedHash = Convert.ToBase64String(
+                hmac.ComputeHash(Encoding.UTF8.GetBytes(password))
+            );
+
+            return computedHash == storedHash;
+        }
     }
 }
